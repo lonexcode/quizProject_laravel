25e238e2c524ac5606304af3a9aca06a8eee6f13
ajayxcode <ajayxlearn@gmail.com>
2025-06-14 23:34:00 +0530

    second commit testing

--- resources/views/user-login.blade.php
+++ resources/views/user-login.blade.php
@@ -42,7 +42,7 @@
             </div>
 
             <div>
-                <label for="password" class="text-gray-600 mb-1 block">Password</label>
+                <label for="password" class="text-gray-800 mb-1 block">Password</label>
                 <input type="password" name="password" id="password" placeholder="Enter your password"
                     class="w-full px-4 py-2 border border-gray-300 rounded-2xl focus:outline-none focus:ring-2 focus:ring-blue-400" />
                 @error('password')
